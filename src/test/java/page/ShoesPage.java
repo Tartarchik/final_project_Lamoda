8c696f60adb0c8e7415408998e7803113c07be78
renat <renat1703@gmail.com>
2023-06-09 18:47:53 +0300

    Fix

--- src/test/java/page/ShoesPage.java
+++ src/test/java/page/ShoesPage.java
@@ -11,23 +11,11 @@ import static com.codeborne.selenide.Selenide.$$;
 
 public class ShoesPage {
     private final SelenideElement shoesSection = $("a[href='/c/17/shoes-men/?sitelink=topmenuM&l=4']");
-    private final SelenideElement shoes = $("._area_552z7_8");
-    private final SelenideElement sizeShoes = $$("._root_1d79y_2").first().$("div");
     private final ElementsCollection filterShoesList = $$("._filters_py6ob_19 span");
 
 
     public ShoesPage openTheShoesSection() {
         shoesSection.click();
-        return this;
-    }
-
-    public ShoesPage chooseShoes() {
-        shoes.click();
-        return this;
-    }
-
-    public ShoesPage chooseSizeShoes() {
-        sizeShoes.click();
         return this;
     }
 
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -20,7 +20,7 @@ public class BaseTest {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion","100");
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
-//        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
